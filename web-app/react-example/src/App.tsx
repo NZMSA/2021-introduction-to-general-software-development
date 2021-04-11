276a74fe2afbaabe08c9036ec86a5619353b5b74
road2paradise <ken_nggg@outlook.com>
2021-04-11 13:23:15 +1200

    WIP counter application working

--- web-app/react-example/src/App.tsx
+++ web-app/react-example/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Counter from './components/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Counter counterName="some funny string"/>
     </div>
   );
 }
--- web-app/react-example/src/components/Counter.tsx
+++ web-app/react-example/src/components/Counter.tsx
@@ -1,1 +1,65 @@
 import React from 'react';
+import "./Counter.css"
+
+interface CounterProps {
+    counterName?: string;
+}
+
+
+const Counter: React.FC<CounterProps> = (props) => {
+    // State changes here
+    const [count, setCount] = React.useState(5);
+    const [someString, setSomeString] = React.useState<string | null>(null)
+    const [imageUrl, setImageUrl] = React.useState<string | null>(null)
+
+    const increment = () => {
+        setCount(count+1)
+    }
+
+    const decrement = () => {
+        setCount(count-1)
+    }
+
+    React.useEffect(() => {
+        if (count >= 10) {
+            setSomeString("Congratulations you can increment the button 10 times");
+            setImageUrl("/super-secret-image.jpg")
+        } else if (count <= 0) {
+            setSomeString("Bruh stop decrementing");
+            setImageUrl("/DO-NOT-OPEN.png")
+        } else {
+            setSomeString(null)
+            setImageUrl(null)
+        }
+    }, [count]);
+
+    React.useEffect(() => {
+        alert("I have rendered :)");
+        return () => {
+            alert("I have stopped rendering :)")
+        }
+    }, [])
+
+    return <div className="counter-wrapper">
+        <div className="counter">
+            { /* Show multiple onClick examples, show mistakes*/}
+            <button className="counter-btn" onClick={decrement}>
+                {"<"}
+            </button>
+            <div className="counter-label">
+                {someString ?? `${props.counterName}:  ${count}`}
+            </div>
+            <button className="counter-btn" onClick={ increment }>
+                {">"}
+            </button>
+        </div>
+        <div className="counter-image">
+            {imageUrl ? <img alt="some string" src={imageUrl}/> : null}
+        </div>
+    </div>
+}
+
+Counter.defaultProps = {
+    counterName: "some counter name"
+}
+export default Counter;
