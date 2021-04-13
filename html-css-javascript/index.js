d39ff66d6077c248da382ced765578b479c957d3
ZHENK <chengzhenyang@gmail.com>
2021-04-14 00:32:31 +1200

    Update presentation and add JS

---
+++ html-css-javascript/index.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+const notLoaded = document.getElementById("btn");
+console.log(notLoaded);
+
+window.onload = () => {
+  const btn = document.getElementById("btn");
+  btn.innerHTML = "Red";
+  btn.style = "color: red";
+  console.log(btn);
+
+  let blue = false;
+
+  const handleClick = () => {
+    console.log("I clicked my btn");
+    if (blue) {
+      btn.innerHTML = "Red";
+      btn.style = "color: red";
+      blue = false;
+    } else {
+      btn.innerHTML = "Blue";
+      btn.style = "color: blue";
+      blue = true;
+    }
+  };
+
+  btn.addEventListener("click", handleClick);
+};
